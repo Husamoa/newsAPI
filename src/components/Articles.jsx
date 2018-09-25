8a429ecf2a2ce8b2ece966a6f22df2cbd65d68cc
Husamoa <pawebial@gmail.com>
2018-09-25 19:29:22 +0200

    Technology articles in cards

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getData } from './server';
+import { getData } from './Data';
 
 export default class Articles extends Component {
     constructor(props) {
@@ -11,6 +11,10 @@ export default class Articles extends Component {
 
     componentWillMount = () => {
         this.getArticles();
+    }
+
+    componentWillUnmount= () => {
+        this.isCancelled = true;
     }
 
     formatDate(date) {
@@ -37,7 +41,7 @@ export default class Articles extends Component {
         getData().then(res => {
             const articles = res.articles
             console.log(articles)
-            this.setState({
+            !this.isCancelled && this.setState({
                 articles: articles
             })
         })
@@ -45,30 +49,33 @@ export default class Articles extends Component {
 
     render() {
         return (
-            <div className="cardsContainer">
-                {this.state.articles.map((news, i) => {
-                    return (
-                        <div className="card" key={i}>
-                            <div className="content">
-                                <h3>
-                                    <a href={news.url} target="_blank">
-                                        {news.title}
-                                    </a>
-                                </h3>
-                                <p>{news.description}</p>
-                                <div className="author">
-                                    <p>
-                                        By <i>{news.author ? news.author : this.props.default}</i>
-                                    </p>
-                                    <p>{this.formatDate(news.publishedAt)}</p>
+            <div className='container'>
+                <div className='row'>
+                    {this.state.articles.map((news, i) => {
+                        return (
+                            <div className='col-lg-6' key={i}>
+                                <div className="card" style={{ width: '30rem' }}>
+                                    {news.urlToImage ? <img className='card-img-top' style={{ height: '300px' }} src={news.urlToImage} alt="No image" /> : null}
+                                    <div className="card-body">
+                                        <h5 className='card-title'>
+                                            {news.title}
+                                        </h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">
+                                            <p>
+                                                <i>{news.author ? news.author : this.props.default}</i>
+                                            </p>
+                                            <p>{this.formatDate(news.publishedAt)}</p>
+                                        </h6>
+                                        <p className='card-text'>{
+                                            news.description
+                                            }</p>
+                                        <a href={news.url} target='_blank' className="btn btn-primary">Przejdź do artykułu</a>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="image">
-                                <img src={news.urlToImage} alt="" />
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
         );
     }
--- src/components/Data.js
+++ src/components/Data.js
@@ -10,3 +10,4 @@ export const getData = () => {
         return res
     });    
 }
+
